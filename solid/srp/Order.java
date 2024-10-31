0672fdca0132c84dd4c6f2757c4e5c1042368498
ashaury <shaury.lamb@gmail.com>
2024-11-01 06:08:30 +0700

    not srp

---
+++ solid/srp/Order.java
@@ -0,0 +1,14 @@
+class Order{
+    public void calculateTotalSum(){
+
+    }
+    public void getItems(){
+
+    }
+    public void getItemCount(){
+
+    }
+    public void addItem(Item item){
+
+    }
+}
